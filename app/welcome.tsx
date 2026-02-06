790c11075ed4966f3657bc6ded112fce14485dd5
Mohammad Imran <raining101290@gmail.com>
2026-02-06 08:13:37 +0600

    welcome page

---
+++ app/welcome.tsx
@@ -0,0 +1,10 @@
+import ScreenWrapper from "@/components/ScreenWrapper";
+import { Text } from "react-native";
+
+export default function Index() {
+  return (
+    <ScreenWrapper bg="black">
+        <Text className="text-black">Welcome</Text>
+    </ScreenWrapper>
+  );
+}
